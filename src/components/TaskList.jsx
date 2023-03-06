97f5e80dda9586c4b3fe3e1f03b4de1b4440589d
Anay Sharma <anaysharma8791@gmail.com>
2023-03-06 16:38:34 +0530

    add taskList componenent

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,12 @@
+import { TaskItem } from './TaskItem';
+import styles from './TaskList.module.css';
+
+export const TaskList = ({ tasks }) => {
+	return (
+		<ul className={styles.tasks}>
+			{tasks.map((task) => {
+				<TaskItem key={task.id} task={task} />;
+			})}
+		</ul>
+	);
+};
