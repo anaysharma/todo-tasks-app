b797f464ce276339a6d7127288f20c95e70dc703
Anay Sharma <anaysharma8791@gmail.com>
2023-03-06 16:23:01 +0530

    add customForm component

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
+
 export const CustomForm = () => {
+	const [task, setTask] = useState('');
+
 	const handleFormSubmit = (e) => {
 		e.preventDefault();
-		console.log(e);
+		setTask('');
 	};
+
 	return (
 		<form className="todo" onSubmit={handleFormSubmit}>
 			<div className="wrapper">
-				<input type="text" id="task" className="input" />
+				<input
+					type="text"
+					id="task"
+					className="input"
+					value={task}
+					onInput={(e) => setTask(e.target.value)}
+					required
+					autoFocus
+					maxLength={60}
+					placeholder="Enter Task"
+				/>
+				<label htmlFor="task" className="label">
+					Enter Task
+				</label>
 			</div>
+			<button className="btn" aria-label="Add Task" type="submit">
+				<i className="h h-plus"></i>
+			</button>
 		</form>
 	);
 };
